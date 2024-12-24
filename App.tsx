3b1b3b5660c2c5c57f175a340cf3e2b608599af9
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-12-25 04:09:45 +0530

    Add developer credit text to App component

--- App.tsx
+++ App.tsx
@@ -5,8 +5,12 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text style={{fontSize:40,fontWeight:'bold',marginTop:100,marginBottom:50}}>Currency Convertor</Text>
-        <CurrencyConvertor/>
+        <Text style={{ fontSize: 40, fontWeight: 'bold', marginTop: 100, marginBottom: 50 }}>Currency Convertor</Text>
+        <CurrencyConvertor />
+
+      </View>
+      <View style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
+        <Text style={{ fontSize: 10 }}>Developed by: teshan</Text>
       </View>
     </SafeAreaView>
   );
